631da9b9ae56c3c7771bb8702b9776af6aec91f5
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-21 21:12:35 -0700

    Create Train _model.py

---
+++ VecLSTM/LSTM model/Train _model.py
@@ -0,0 +1,15 @@
+;==========================================
+; Title: Train the model with class weights
+; Author: 
+; Date:   21 March 2024
+;==========================================
+
+history = model.fit(
+    X_train_scaled_reshaped,
+    y_train_encoded,
+    epochs=20,  
+    batch_size=64,
+    validation_split=0.2,
+    callbacks=[early_stopping],
+    class_weight=class_weight_dict
+)
