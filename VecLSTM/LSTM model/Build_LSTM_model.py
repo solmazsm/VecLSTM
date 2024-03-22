9d2bd80ffa114082361791804f412b4b35d7ec07
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-21 23:26:58 -0700

    Update Build_LSTM_model.py

--- VecLSTM/LSTM model/Build_LSTM_model.py
+++ VecLSTM/LSTM model/Build_LSTM_model.py
@@ -1,6 +1,6 @@
 ;==========================================
 ; Title: Build_LSTM_model
-; Author: 
+; Author: anonymous - ECML PKDD 2024 - conference
 ; Date:   21 March 2024
 ;==========================================
 
