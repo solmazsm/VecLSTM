18b8c7a0a6b2748d5dc52ee2dfe67d719c906481
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-21 20:53:36 -0700

    Create VecLSTM_import.py

---
+++ importing modules/keras.models/VecLSTM_import.py
@@ -0,0 +1,15 @@
+;==========================================
+; Title: VecLSTM_import
+; Author: Solmaz Seyed Monir
+; Date:   21 March 2024
+;==========================================
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.utils import to_categorical
+import numpy as np
+from sklearn.metrics import classification_report
