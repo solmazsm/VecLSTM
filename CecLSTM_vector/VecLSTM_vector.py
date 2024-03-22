0aef760eee30fce7542bc70a094805a0f52bb100
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-21 22:00:43 -0700

    Create VecLSTM_vector.py

---
+++ CecLSTM_vector/VecLSTM_vector.py
@@ -0,0 +1,10 @@
+;==========================================
+; Title: vectorization
+; Author: 
+; Date:   21 March 2024
+;==========================================
+
+vectorized_data_array = VecLSTM_vectorization_function(trajectories)
+
+# inspect the vectorized data
+print(vectorized_data_array)
