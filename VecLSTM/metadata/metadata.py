6522ff011feb1f0ceae56ae072762fb6f5f924b6
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-21 21:29:52 -0700

    Create metadata.py

---
+++ VecLSTM/metadata/metadata.py
@@ -0,0 +1,10 @@
+;==========================================
+; Title: Create metadata
+; Author: 
+; Date:   21 March 2024
+;==========================================
+
+metadata_list = [f"{trajectory_id}_{point}" for trajectory_id, trajectory in enumerate(trajectories) for point in range(len(trajectory))]
+
+# Create labels and metadata
+df_true_labels = pd.DataFrame({'Metadata': metadata_list, 'TrueLabel': labels_list})
