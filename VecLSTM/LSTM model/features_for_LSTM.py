c5e699fe75629e4484cd601ad9e94188f1a601b9
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-21 21:10:25 -0700

    Create features_for_LSTM.py

---
+++ VecLSTM/LSTM model/features_for_LSTM.py
@@ -0,0 +1,8 @@
+;==========================================
+; Title: Reshape the input features for LSTM
+; Author: 
+; Date:   21 March 2024
+;==========================================
+
+X_train_scaled_reshaped = X_train_scaled.reshape(-1, X_train_scaled.shape[1], 1)
+X_test_scaled_reshaped = X_test_scaled.reshape(-1, X_test_scaled.shape[1], 1)
