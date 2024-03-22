421696a7dd5c5d86be4a1fa5674adf4c478fc226
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-21 21:50:58 -0700

    Create VecLSTM_vector_database_query.py

---
+++ Measure the time for vector database query/VecLSTM_vector_database_query.py
@@ -0,0 +1,7 @@
+;==========================================
+; Title:  Measure the time for vector database query
+; Author: 
+; Date:   21 March 2024
+;==========================================
+
+start_time_vector_db = time.time()
