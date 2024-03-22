5ee71eff760cc4cb233d721231c0163e1c1c82a4
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-21 23:27:49 -0700

    Update metadata.py

--- VecLSTM/metadata/metadata.py
+++ VecLSTM/metadata/metadata.py
@@ -1,6 +1,6 @@
 ;==========================================
 ; Title: Create metadata
-; Author: 
+; Author: anonymous - ECML PKDD 2024 - conference
 ; Date:   21 March 2024
 ;==========================================
 
