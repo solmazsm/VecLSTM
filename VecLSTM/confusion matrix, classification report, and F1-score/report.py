83a837c1c016f324d0c3549bfe28e55505c554df
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-21 23:27:59 -0700

    Update report.py

--- VecLSTM/confusion matrix, classification report, and F1-score/report.py
+++ VecLSTM/confusion matrix, classification report, and F1-score/report.py
@@ -1,6 +1,6 @@
 ;==========================================
 ; Title: confusion matrix, classification report, and F1-score
-; Author: 
+; Author: anonymous - ECML PKDD 2024 - conference
 ; Date:   21 March 2024
 ;==========================================
 conf_matrix = confusion_matrix(np.argmax(y_test_encoded, axis=1), predictions_array)
