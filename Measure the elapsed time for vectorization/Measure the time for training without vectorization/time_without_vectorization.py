059ab6bb2e39478dde93d904e5e2672a80b8da8b
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-21 22:30:02 -0700

    Create time_without_vectorization.py

---
+++ Measure the elapsed time for vectorization/Measure the time for training without vectorization/time_without_vectorization.py
@@ -0,0 +1,7 @@
+;==========================================
+; Title: Measure the time for training without vectorization
+; Author: 
+; Date:   21 March 2024
+;==========================================
+
+start_time_without_vectorization = time.time()
