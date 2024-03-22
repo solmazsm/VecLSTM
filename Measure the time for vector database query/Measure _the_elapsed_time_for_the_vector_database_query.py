632de07092c728ae84f343cad991bfc37353bb00
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-21 21:54:27 -0700

    Create Measure _the_elapsed_time_for_the_vector_database_query.py

---
+++ Measure the time for vector database query/Measure _the_elapsed_time_for_the_vector_database_query.py
@@ -0,0 +1,8 @@
+;==========================================
+; Title: Measure the elapsed time for the vector database query
+; Author: 
+; Date:   21 March 2024
+;==========================================
+
+end_time_vector_db = time.time()
+vector_database_query_time = end_time_vector_db - start_time_vector_db
