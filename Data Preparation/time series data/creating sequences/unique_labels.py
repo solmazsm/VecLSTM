1685302d78346364fd04c42262c4325669fade07
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-21 20:49:45 -0700

    Create unique_labels.py

---
+++ Data Preparation/time series data/creating sequences/unique_labels.py
@@ -0,0 +1,8 @@
+;==========================================
+; Title: unique_labels
+; Author: Solmaz Seyed Monir
+; Date:   21 March 2024
+;==========================================
+
+label_counts = subset_df['label'].value_counts()
+print(label_counts)
