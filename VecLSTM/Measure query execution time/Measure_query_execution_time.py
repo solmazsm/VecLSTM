25e11f87a89ecdf4c56d425e8524a1414eee1a27
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-21 21:36:10 -0700

    Create Measure_query_execution_time.py

---
+++ VecLSTM/Measure query execution time/Measure_query_execution_time.py
@@ -0,0 +1,8 @@
+;==========================================
+; Title: Measure query execution time
+; Author: 
+; Date:   21 March 2024
+;==========================================
+
+query_execution_time = end_time - start_time
+print(f"Query execution time: {query_execution_time} seconds")
