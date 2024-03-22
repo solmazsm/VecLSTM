cf9707465f6706264f2188254b58587580153ff4
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-21 21:06:17 -0700

    Create Build_LSTM_model.py

---
+++ VecLSTM/LSTM model/Build_LSTM_model.py
@@ -0,0 +1,10 @@
+;==========================================
+; Title: Build_LSTM_model
+; Author: 
+; Date:   21 March 2024
+;==========================================
+
+model = Sequential()
+model.add(LSTM(units=100, input_shape=(X_train_scaled_reshaped.shape[1], X_train_scaled_reshaped.shape[2]), return_sequences=True))
+model.add(LSTM(units=50, return_sequences=False))  
+model.add(Dense(units=num_classes, activation='softmax'))
