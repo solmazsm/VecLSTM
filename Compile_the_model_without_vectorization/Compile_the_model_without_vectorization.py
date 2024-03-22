087008c181323c7634581bb54f721c3a653f28ad
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-21 23:25:12 -0700

    Create Compile_the_model_without_vectorization.py

---
+++ Compile_the_model_without_vectorization/Compile_the_model_without_vectorization.py
@@ -0,0 +1,13 @@
+;==========================================
+; Title: Compile_the_model_without_vectorization
+; Author: anonymous - ECML PKDD 2024 - conference
+; Date:   21 March 2024
+;==========================================
+
+    model_without_vectorization = Sequential()
+    model_without_vectorization.add(LSTM(100, input_shape=(X_train_resampled.shape[1], 1), activation='relu', return_sequences=True))
+    model_without_vectorization.add(LSTM(50, activation='relu'))
+    model_without_vectorization.add(Dense(num_classes, activation='softmax'))
+
+    # Compile the model
+    model_without_vectorization.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
