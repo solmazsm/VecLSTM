cbc4cdfdd911a303fa39174ac72be2cd8211168d
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-03-21 22:26:50 -0700

    Create VecLSTM_time_vectorization.py

---
+++ Measure the elapsed time for vectorization/VecLSTM_time_vectorization.py
@@ -0,0 +1,8 @@
+;==========================================
+; Title: Measure the elapsed time for vectorization
+; Author:
+; Date:   21 March 2024
+;==========================================
+
+end_time_vectorization = time.time()
+vectorization_time = end_time_vectorization - start_time_vectorization
