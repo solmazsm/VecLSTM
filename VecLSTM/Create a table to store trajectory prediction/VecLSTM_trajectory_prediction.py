e766e76d2ac6e05f21d0a95c31873ca13355be41
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-06-19 11:47:32 -0700

    Update VecLSTM_trajectory_prediction.py

--- VecLSTM/Create a table to store trajectory prediction/VecLSTM_trajectory_prediction.py
+++ VecLSTM/Create a table to store trajectory prediction/VecLSTM_trajectory_prediction.py
@@ -1,7 +1,7 @@
 ;==========================================
 ; Title: Create a table to store VecLSTM_trajectory_prediction
-; Author: anonymous - ECML PKDD 2024 - conference
-; Date:   21 March 2024
+; Author: anonymous - ICDM 2024 - conference
+; Date:   ICDM 2024
 ;==========================================
 
 create_table_query = 
