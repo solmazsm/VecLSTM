cb0d969c8bbff1196b9b600a9e1a972f11c17164
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-06-19 13:04:24 -0700

    Create measure_time_vectorization.py

---
+++ vectorization/vectorization_function/Measure_time_vectorization/measure_time_vectorization.py
@@ -0,0 +1,21 @@
+;==========================================
+; Title: Measure the time for vectorization
+; Author: anonymous - ICDM 2024 - conference
+; Date:   ICDM 2024
+;==========================================
+# With Vectorization
+    # Measure the time for vectorization
+    start_time_vectorization = time.time()
+
+   
+    vectorized_data_array = np.zeros((len(trajectories), 10, 10, len(trajectories[0][0])))
+
+    for i, trajectory in enumerate(trajectories):
+        vectorized_data_array[i] = vectorization_function(trajectory)
+
+   
+    end_time_vectorization = time.time()
+    vectorization_time = end_time_vectorization - start_time_vectorization
+
+   
+   
