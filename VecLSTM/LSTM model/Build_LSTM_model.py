b6fdc24aff0fafdbb2e233f18e4035813d3b8a3d
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-06-19 11:48:28 -0700

    Update Build_LSTM_model.py

--- VecLSTM/LSTM model/Build_LSTM_model.py
+++ VecLSTM/LSTM model/Build_LSTM_model.py
@@ -1,7 +1,7 @@
 ;==========================================
 ; Title: Build_LSTM_model
-; Author: anonymous - ECML PKDD 2024 - conference
-; Date:   21 March 2024
+; Author: anonymous - ICDM 2024 - conference
+; Date:   ICDM 2024
 ;==========================================
 
 model = Sequential()
