3fe1403218fd731e77df284029fca9d52223502f
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-06-19 11:46:47 -0700

    Update VecLSTM_vector_database_query.py

--- Measure the time for vector database query/VecLSTM_vector_database_query.py
+++ Measure the time for vector database query/VecLSTM_vector_database_query.py
@@ -1,7 +1,7 @@
 ;==========================================
 ; Title:  Measure the time for vector database query
-; Author: anonymous - ECML PKDD 2024 - conference
-; Date:   21 March 2024
+; Author: anonymous - ICDM 2024 - conference
+; Date:  ICDM 2024
 ;==========================================
 
 start_time_vector_db = time.time()
