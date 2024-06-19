f371e1d4854d0e9a0827b8a2646319227797926d
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-06-19 11:49:02 -0700

    Update Train _model.py

--- VecLSTM/LSTM model/Train _model.py
+++ VecLSTM/LSTM model/Train _model.py
@@ -1,7 +1,7 @@
 ;==========================================
 ; Title: Train the model with class weights
-; Author: anonymous - ECML PKDD 2024 - conference
-; Date:   21 March 2024
+; Author: anonymous - ICDM 2024 - conference
+; Date:   ICDM 2024
 ;==========================================
 
 history = model.fit(
