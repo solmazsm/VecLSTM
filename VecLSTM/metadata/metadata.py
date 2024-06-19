787c3dde771d541365e14f9c579f229a71d09243
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-06-19 11:51:51 -0700

    Update metadata.py

--- VecLSTM/metadata/metadata.py
+++ VecLSTM/metadata/metadata.py
@@ -1,7 +1,7 @@
 ;==========================================
 ; Title: Create metadata
-; Author: anonymous - ECML PKDD 2024 - conference
-; Date:   21 March 2024
+; Author: anonymous - ICDM 2024 - conference
+; Date:   ICDM 2024
 ;==========================================
 
 metadata_list = [f"{trajectory_id}_{point}" for trajectory_id, trajectory in enumerate(trajectories) for point in range(len(trajectory))]
