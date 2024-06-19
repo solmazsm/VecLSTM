0547394827de98453caa70c89529974e197f9cf5
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-06-19 11:45:35 -0700

    Update unique_labels.py

--- Data Preparation/time series data/creating sequences/unique_labels.py
+++ Data Preparation/time series data/creating sequences/unique_labels.py
@@ -1,7 +1,7 @@
 ;==========================================
 ; Title: unique_labels
-; Author: anonymous - ECML PKDD 2024 - conference
-; Date:   21 March 2024
+; Author: anonymous - ICDM 2024 - conference
+; Date:   ICDM 2024
 ;==========================================
 
 label_counts = subset_df['label'].value_counts()
