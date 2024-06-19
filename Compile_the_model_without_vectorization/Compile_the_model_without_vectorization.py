da166390de215c59d36f77c142bca3b380a5a5ce
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-06-19 11:44:58 -0700

    Update Compile_the_model_without_vectorization.py

--- Compile_the_model_without_vectorization/Compile_the_model_without_vectorization.py
+++ Compile_the_model_without_vectorization/Compile_the_model_without_vectorization.py
@@ -1,7 +1,7 @@
 ;==========================================
 ; Title: Compile_the_model_without_vectorization
-; Author: anonymous - ECML PKDD 2024 - conference
-; Date:   21 March 2024
+; Author: anonymous - ICDM 2024 - conference
+; Date:   ICDM 2024
 ;==========================================
 
     model_without_vectorization = Sequential()
