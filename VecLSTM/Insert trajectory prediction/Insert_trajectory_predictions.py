e9b198023a00dfc56a7326efbb9e01fce6be4c26
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-06-19 11:47:50 -0700

    Update Insert_trajectory_predictions.py

--- VecLSTM/Insert trajectory prediction/Insert_trajectory_predictions.py
+++ VecLSTM/Insert trajectory prediction/Insert_trajectory_predictions.py
@@ -1,7 +1,7 @@
 ;==========================================
 ; Title: Insert trajectory predictions into a MySQL table
-; Author: anonymous - ECML PKDD 2024 - conference
-; Date:  21 March 2024
+; Author: anonymous - ICDM 2024 - conference
+; Date:  ICDM 2024
 ;==========================================
 for index, row in df_predictions.iterrows():
     query = f"INSERT INTO trajectory_predictions (metadata, prediction) VALUES ('{row['Metadata']}', {row['Prediction']})"
