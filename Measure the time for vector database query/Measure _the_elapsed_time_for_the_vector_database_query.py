6969ffea8c9dd04be00e2450f6bb8671bebe6014
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-06-19 11:46:14 -0700

    Update Measure _the_elapsed_time_for_the_vector_database_query.py

--- Measure the time for vector database query/Measure _the_elapsed_time_for_the_vector_database_query.py
+++ Measure the time for vector database query/Measure _the_elapsed_time_for_the_vector_database_query.py
@@ -1,7 +1,7 @@
 ;==========================================
 ; Title: Measure the elapsed time for the vector database query
-; Author: anonymous - ECML PKDD 2024 - conference
-; Date:   21 March 2024
+; Author: anonymous - ICDM 2024 - conference
+; Date:   ICDM 2024
 ;==========================================
 
 end_time_vector_db = time.time()
