65e3a00380ead804eaae2d64cb1216b5a253ca1a
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-06-19 12:24:43 -0700

    Create vectorized_data.py

---
+++ VecLsm/vector/vectorized_data.py
@@ -0,0 +1,12 @@
+;==========================================
+; Title: Create a vector representation
+; Author: anonymous - ICDM 2024 - conference
+; Date:   ICDM 2024
+;========================================== 
+
+# This function takes the latitude (lat), longitude (lon), and time (time) columns 
+# transforms them into a format suitable for further processing
+# input into a machine learning model
+# The resulting vectorized_data variable holds these transformed vector representations of the original geographical and temporal data points
+
+vectorized_data = create_vector_representation(dfv['lat'], dfv['lon'], dfv['time'])
