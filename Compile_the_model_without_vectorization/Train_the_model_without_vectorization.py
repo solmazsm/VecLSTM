57b2e4863106262fdba34d5d86349e40ab3bb32e
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-06-19 11:45:15 -0700

    Update Train_the_model_without_vectorization.py

--- Compile_the_model_without_vectorization/Train_the_model_without_vectorization.py
+++ Compile_the_model_without_vectorization/Train_the_model_without_vectorization.py
@@ -1,7 +1,7 @@
 ;==========================================
 ; Title: Train_the_model_without_vectorization
-; Author: anonymous - ECML PKDD 2024 - conference
-; Date:   21 March 2024
+; Author: anonymous - ICDM 2024 - conference
+; Date:   ICDM 2024
 ;==========================================
 
 model_without_vectorization.fit(
