196fcd86d196ee723b28d2a612715fa17cbb2d8e
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-12-04 10:52:39 -0800

    Update results.py

--- Measure the time for vector database query/results.py
+++ Measure the time for vector database query/results.py
@@ -1,7 +1,6 @@
 ;==========================================
 ; Title: Compare times and visualize the results
-; Author: 
-; Date:   ICDM 2024
+
 ;==========================================
 
 comparison_data = {'Vectorization Time': vectorization_time, 'Vector DB Query Time': vector_database_query_time}
