8b69617b62690d8bd9f922c03d26ab8b7b9b1d59
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-12-04 10:51:52 -0800

    Update VecLSTM_time_vectorization.py

--- Measure the elapsed time for vectorization/VecLSTM_time_vectorization.py
+++ Measure the elapsed time for vectorization/VecLSTM_time_vectorization.py
@@ -1,7 +1,6 @@
 ;==========================================
 ; Title: Measure the elapsed time for vectorization
-; Author: anonymous - ICDM 2024 - conference
-; Date:   ICDM 2024
+; Author: anonymous 
 ;==========================================
 
 end_time_vectorization = time.time()
