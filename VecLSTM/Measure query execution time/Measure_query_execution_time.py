2633bd20c2a914e7c42bb47ca5bff3d91998568a
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-12-04 10:49:01 -0800

    Update Measure_query_execution_time.py

--- VecLSTM/Measure query execution time/Measure_query_execution_time.py
+++ VecLSTM/Measure query execution time/Measure_query_execution_time.py
@@ -1,7 +1,7 @@
 ;==========================================
 ; Title: Measure query execution time
-; Author: anonymous - ICDM 2024 - conference
-; Date:   ICDM 2024
+; Author: anonymous 
+; Date:    The Web Conference 2025
 ;==========================================
 
 query_execution_time = end_time - start_time
